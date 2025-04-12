92c4320eacb0885e2200a3a0e78aac873699975c
Alaa-shorman <alaa2233300@gmail.com>
2025-04-12 21:27:53 +0300

    ALAA

---
+++ frontend/main/java/home.js
@@ -0,0 +1,21 @@
+
+ 
+
+
+function openPopup() {
+  document.getElementById("popupWindow").style.display = "flex";
+}
+
+function closePopup() {
+  document.getElementById("popupWindow").style.display = "none";
+}
+
+
+
+
+// عملت القائمه الجانبيه هون 
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('active');
+  }
+  
