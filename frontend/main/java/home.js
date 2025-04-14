7f0d9fc871dfc2b8c28ef7e07be6787ab56f7af9
mareiafawares <mareiafawares04@gmail.com>
2025-04-15 02:33:18 +0300

    اخر تعديلات

--- frontend/main/java/home.js
+++ frontend/main/java/home.js
@@ -11,23 +11,39 @@ function toggleSidebar() {
 
 // النافذة المنبثقة
 function openPopup() {
-  const popup = document.getElementById('popupWindow');
+  const popup = document.getElementById('popupWindow1');
   if (popup) {
     popup.style.display = 'flex';
   }
 }
 
-function closePopup() {
-  const popup = document.getElementById('popupWindow');
+function closePopup1() {
+  const popup = document.getElementById('popupWindow1');
   if (popup) {
     popup.style.display = 'none';
   }
 }
 
-// إغلاق النافذة عند الضغط خارجها 
+// إغلاق النافذة عند الضغط خارجها (اختياري)
 window.addEventListener('click', function (event) {
-  const popup = document.getElementById('popupWindow');
+  const popup = document.getElementById('popupWindow1');
   if (popup && event.target === popup) {
     closePopup();
   }
 });
+function openPopupWithPage(pageUrl) {
+  const popup = document.getElementById('popupWindow');
+  const iframe = popup.querySelector('iframe');
+  iframe.src = pageUrl;
+  popup.style.display = 'flex';
+}
+
+function closePopup() {
+  const popup = document.getElementById('popupWindow');
+  const iframe = popup.querySelector('iframe');
+  iframe.src = '';
+  popup.style.display = 'none';
+}
+
+// تفعيل الوضع الليلي
+
