b53ba860ff06faff602de7b527cbea16faba4a68
farahhowari <farahhowari783@gmail.com>
2025-04-30 19:23:29 +0300

    sign up

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors'); 
 const connectDB = require('./config/db'); 
 const userRouters = require('./routers/userRouters');
 dotenv.config();
 
-
 connectDB();
-
 
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:5501', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use(express.json());
 
-
 app.use('/api', userRouters);
 
-
 const PORT = process.env.PORT || 5500;
-app.listen(PORT, () => console.log(` Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
 console.log("Mongo URI:", process.env.MONGO_URI);
