fc59bd83c54ba7021ececaa677dcbcd00b895746
farahhowari <farahhowari783@gmail.com>
2025-05-14 21:40:38 +0300

    تغيير الlog in

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,9 +6,7 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    role:{  type: String,
-        enum: ['customer', 'manager', 'employee'], 
-        default: 'customer' 
+    role:{  type: String, default: 'customer'||"employee" ,
     }
 });
 
